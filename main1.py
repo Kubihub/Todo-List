5d29af82499a3b4327faf2ee60d81c9c4fbae0d8
Adizatu Alhassan <54992528+Kubihub@users.noreply.github.com>
2023-02-02 13:42:38 +0100

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,10 @@
+user_prompt = 'Enter a todo list:'
+todo1 = input(user_prompt)
+todo2 = input(user_prompt)
+todo3 = input(user_prompt)
+
+todos = [todo3, todo2, todo1, 'Hello']
+
+print(todos)
+
+print(type(todos)) 
