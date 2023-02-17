00f07a2534b21d8fb955e1c230783e11c1c92240
Adiza Alhassan <54992528+Kubihub@users.noreply.github.com>
2023-02-17 11:46:52 +0100

    Update main1.py
    I added a strip () method that eliminates any other white space that is entered during the user input.
    And there is a matching case that makes it possible to iterate when a certain value is inputted.
    And there is the for loop with the match case that contains a title() method that removes all the bracket that comes when it's printed out of the list.
    And lastly, the last case executes when non of the value input is recognized by the match case

--- main1.py
+++ main1.py
@@ -3,14 +3,19 @@ todos = []
 
 while True:
     user_action = input("Type add,exit or show")
+    user_action = user_action.strip()
     match user_action:
         case "add":
             todo = input("Enter a todo")
             todos.append(todo)
         case "show":
-            print(todos)
+            for item in todos:
+                item = item.title()
+                print(item)
         case "exit":
             break
+        case incorrect_command:
+            print("please enter the correct command again")
 
 
 print("bye")
