74520045579183d4155161212b9ad185a121100a
Adiza Alhassan <54992528+Kubihub@users.noreply.github.com>
2023-02-25 13:24:14 +0100

    Update main1.py
    I added a complete case that is clicked when the todo item is completed and with this, there is a new method that is pop() which removes the todo item from the list when the index number of that item is inputted

--- main1.py
+++ main1.py
@@ -2,7 +2,7 @@ todos = []
 
 
 while True:
-    user_action = input("Type add,exit or show")
+    user_action = input("Type add,show, edit,complete or exit")
     user_action = user_action.strip()
     match user_action:
         case "add":
@@ -10,12 +10,18 @@ while True:
             todos.append(todo)
         case "show":
             for index, item in enumerate(todos):
-                row = f"{index}->{item}"
+                row = f"{index + 1}-{item}"
                 print(row)
+        case "edit":
+            number = int(input("number of the todo item to edit:"))
+            number = number - 1
+            new_todo = input("please input the correct todo")
+            todos[number] = new_todo
+
+        case "complete":
+            number = int(input("Number of the todo that is completed"))
+            todos.pop(number - 1)
         case "exit":
             break
-        case incorrect_command:
-            print("please enter the correct command again")
-
 
 print("bye")
